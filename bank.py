cdffad99a935ca798813fe0d483e793b8714d25e
JanuarRizki30 <113058956+JanuarRizki30@users.noreply.github.com>
2022-09-18 20:40:00 +0700

    Create bank.py

---
+++ bank.py
@@ -0,0 +1,12 @@
+grt=input(' ').lower().replace(" ","")
+z=grt[0:5]
+if(grt=='hello'):
+    money=0
+elif(z=='hello'):
+    money=0
+elif(grt[0]=='h'):
+    money=20
+else :
+    money=100
+
+print ('$',money)
